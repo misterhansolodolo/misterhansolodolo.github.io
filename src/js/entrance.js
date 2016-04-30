192007bb336bfa0ec1af795f3ca4dc7f10b90ef9
hyi <hyi@lifetimefitness.com>
2016-04-30 10:24:44 -0600

    test2

--- src/js/entrance.js
+++ src/js/entrance.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $(window).load(function() {
     $('#loading-bar').animate({width: '30%'}, 2000, function() {
       $('#loading-bar').animate({width: '0px'}, 200, function() {
-        window.location.replace(location.hostname + '/header.html')
+        window.location.replace('/about.html');
       });
     });
   });
