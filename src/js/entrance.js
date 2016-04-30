dd6ff916d7210074ac40786850eb0c0a1c556411
hyi <hyi@lifetimefitness.com>
2016-04-30 10:19:50 -0600

    test

--- src/js/entrance.js
+++ src/js/entrance.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $(window).load(function() {
-    $('#loading-bar').animate({width: '30%'}, 3000, function() {
+    $('#loading-bar').animate({width: '30%'}, 2000, function() {
       $('#loading-bar').animate({width: '0px'}, 200, function() {
-        $('body').html('hello')
+        window.location.replace(location.hostname + '/header.html')
       });
     });
   });
