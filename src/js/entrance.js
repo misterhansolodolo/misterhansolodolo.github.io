89862a67d2214fb9d758b4525ed35398b53a920d
hyi <hyi@lifetimefitness.com>
2016-04-30 18:18:06 -0600

    adding favicon

--- src/js/entrance.js
+++ src/js/entrance.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(window).load(function() {
-    $('#loading-bar').animate({width: '30%'}, 2000, function() {
+    $('#loading-bar').animate({width: '30%'}, 3000, function() {
       $('#loading-bar').animate({width: '0px'}, 200, function() {
         window.location.replace('/about.html');
       });
