1c137ce2c89d819f5372c411211e1c1ee936a7f3
hyi <hyi@lifetimefitness.com>
2016-05-01 19:32:21 -0600

    no redirect

--- src/js/entrance.js
+++ src/js/entrance.js
@@ -2,7 +2,44 @@ $(document).ready(function() {
   $(window).load(function() {
     $('#loading-bar').animate({width: '30%'}, 3000, function() {
       $('#loading-bar').animate({width: '0px'}, 200, function() {
-        window.location.replace('/about.html');
+        $('body').html('<img src="./src/images/background.gif" id="gif" />' +
+        '<div id="title">' +
+          '<img src="./src/images/profile.jpg" id="profile" />' +
+          '<div id="name">hello, i&#39;m hansol!</div>' +
+          '<div id="description"> i just want to build cool stuff . . </div>' +
+        '</div>' +
+
+        '<div id="professional-title"> my work </div>' +
+        '<div id="title-bar"> </div>' +
+        '<div id="box-container">' +
+          '<div class="entry-box">' +
+            '<div class="entry-box-front">' +
+
+            '</div>' +
+            '<div class="entry-box-back">' +
+
+            '</div>' +
+          '</div>' +
+          '<div class="entry-box"> </div>' +
+          '<div class="entry-box"> </div>' +
+          '<div class="entry-box"> </div>' +
+        '</div>' +
+
+        '<div id="personal-title"> get to know me </div>' +
+        '<div id="title-bar"> </div>' +
+        '<div id="box-container">' +
+          '<div class="entry-box"> </div>' +
+          '<div class="entry-box"> </div>' +
+          '<div class="entry-box"> </div>' +
+        '</div>' +
+
+
+        '<div id="audio">' +
+          '<audio controls autoplay loop>' +
+            '<source src="./src/music/zhu.mp3" type="audio/mpeg">' +
+          '<audio>' +
+        '</div>');
+        // window.location.replace('/about.html');
       });
     });
   });
